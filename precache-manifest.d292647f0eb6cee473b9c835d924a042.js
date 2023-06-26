2f9f8469fe7dcb4576e4dc765bf46e0e55350051
JAYE YAP <jayeyap.2020@scis.smu.edu.sg>
2023-06-26 22:41:50 +0800

    Deploy React app to GitHub Pages

---
+++ precache-manifest.d292647f0eb6cee473b9c835d924a042.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d355d9cbc3a91df4806",
+    "url": "/static/css/main.76705e29.chunk.css"
+  },
+  {
+    "revision": "5d355d9cbc3a91df4806",
+    "url": "/static/js/main.5d355d9c.chunk.js"
+  },
+  {
+    "revision": "c765bae6be2a5b9a7cdf",
+    "url": "/static/js/1.c765bae6.chunk.js"
+  },
+  {
+    "revision": "57a619db97dc6860fc1d",
+    "url": "/static/css/2.22aca28b.chunk.css"
+  },
+  {
+    "revision": "57a619db97dc6860fc1d",
+    "url": "/static/js/2.57a619db.chunk.js"
+  },
+  {
+    "revision": "7a41c24f38333f284b92",
+    "url": "/static/js/runtime~main.7a41c24f.js"
+  },
+  {
+    "revision": "d9b31adc41fc849ce740e821ee77a645",
+    "url": "/static/media/work-exp.d9b31adc.csv"
+  },
+  {
+    "revision": "2a1dfa53b32135f6241c4c5e10e66a1f",
+    "url": "/static/media/leadership-exp.2a1dfa53.csv"
+  },
+  {
+    "revision": "7615f7b37f01985d370799b061e150be",
+    "url": "/static/media/headshot.7615f7b3.png"
+  },
+  {
+    "revision": "431af9f2a7226e3b721dc22cc092bc2d",
+    "url": "/static/media/projects.431af9f2.csv"
+  },
+  {
+    "revision": "bb85a87e0ad90b3b74528dc50380e7e8",
+    "url": "/static/media/achievements.bb85a87e.csv"
+  },
+  {
+    "revision": "78854ce40e239e15bfd1b4ec1a8fc915",
+    "url": "/static/media/about-me.78854ce4.png"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "a4b745698a7e1b1f981534c57d4e0c4d",
+    "url": "/static/media/scis.a4b74569.png"
+  },
+  {
+    "revision": "f47d9e271fb6d0497eb1ec6f863f32f2",
+    "url": "/index.html"
+  }
+];
